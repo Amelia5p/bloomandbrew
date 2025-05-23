1ab84c762d1f6c8394e1d3b28b631c2fdc24bc80
Amelia <millie-service@hotmail.com>
2025-05-23 11:52:40 +0100

    add order history template and urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('success/<order_number>/', views.checkout_success, name='checkout_success'),
- 
+     path('order-history/', views.order_history, name='order_history'),
 ]
 
