f55b16694d33da22f4b4f5f3c3a8d96fda346876
Amelia <millie-service@hotmail.com>
2025-05-28 10:30:26 +0100

    add product photos to live site

--- bloomandbrewproject/settings.py
+++ bloomandbrewproject/settings.py
@@ -134,9 +134,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
