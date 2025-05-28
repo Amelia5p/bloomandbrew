2f30e356cb75408b4c687895c10132ca5377e68e
Amelia <millie-service@hotmail.com>
2025-05-28 11:26:18 +0100

    update webhook url to match stripe url

--- bloomandbrewproject/settings.py
+++ bloomandbrewproject/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!5u^*goorb&xz!ophk%9d595l77$amr9_ydb1i$ks#=&c8z6a4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost','127.0.0.1', 'bloom-and-brew-2265123bf17d.herokuapp.com', ]
 
 
--- checkout/urls.py
+++ checkout/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('success/<order_number>/', views.checkout_success, name='checkout_success'),
     path('order-history/', views.order_history, name='order_history'),
-    path('webhook/', stripe_webhook, name='stripe_webhook'),
+    path('checkout/wh', stripe_webhook, name='stripe_webhook'),
 ]
 
