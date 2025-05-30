a05087d0ddfaab5af69422b76b0a981d6c373bde
Amelia <millie-service@hotmail.com>
2025-05-30 12:52:17 +0100

    edit the product details page and add styles to improve page flow and ux

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -10,4 +10,12 @@ class ProductAdmin(admin.ModelAdmin):
     ordering = ('sku',)
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'rating', 'comment', 'created_on')
+    list_filter = ('rating', 'product', 'created_on')
+    search_fields = ('user__username', 'product__name', 'comment')
+    ordering = ('-rating', '-created_on')
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
