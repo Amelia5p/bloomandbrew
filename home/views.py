c479324ac9d360ecae3d8c80119586ec27390ad2
Amelia <millie-service@hotmail.com>
2025-05-30 16:48:03 +0100

    rearrange newsletter and bundle section, add image

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from products.models import Product
+import requests
+from django.conf import settings
+from django.contrib import messages
 
-# Create your views here.
 
 def home(request):
     """ A view for the homepage"""
@@ -11,10 +14,7 @@ def about(request):
     """ A view for the about us page"""
     return render (request, 'home/about.html')
 
-from products.models import Product
 
-from django.shortcuts import render
-from products.models import Product
 
 def home(request):
     """ Show homepage with featured products and bundle of the week """
@@ -26,3 +26,36 @@ def home(request):
         'bundle_of_the_week': bundle_of_the_week,
     })
 
+
+# Mailchimp newsletter view
+def newsletter_signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if not email:
+            messages.error(request, "Please enter a valid email.")
+            return redirect('home')
+
+       
+        url = f"https://{settings.MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0/lists/{settings.MAILCHIMP_AUDIENCE_ID}/members"
+
+        payload = {
+            "email_address": email,
+            "status": "subscribed"
+        }
+
+        
+        response = requests.post(
+            url,
+            auth=("anystring", settings.MAILCHIMP_API_KEY),
+            json=payload
+        )
+
+        
+        if response.status_code in [200, 204]:
+            messages.success(request, "Thanks for subscribing!")
+        elif response.status_code == 400 and 'already' in response.text:
+            messages.info(request, "You're already subscribed.")
+        else:
+            messages.error(request, "Something went wrong. Please try again.")
+
+    return redirect('home')
