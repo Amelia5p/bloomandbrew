cc9827ff691d1bcf20aba162f354561b7a0d90df
Amelia <millie-service@hotmail.com>
2025-06-09 09:55:31 +0100

    update confirmation email settings

--- bloomandbrewproject/settings.py
+++ bloomandbrewproject/settings.py
@@ -227,6 +227,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
 
 # Mailchimp API
 MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
